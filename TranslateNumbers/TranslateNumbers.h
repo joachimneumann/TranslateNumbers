8b7897229f6e6c209cb70b051788e0e0fc029409
Joachim Neumann <joachim@joachimneumann.com>
2024-08-23 10:24:32 +0700

    Initial Commit

---
+++ TranslateNumbers/TranslateNumbers.h
@@ -0,0 +1,18 @@
+//
+//  TranslateNumbers.h
+//  TranslateNumbers
+//
+//  Created by Joachim Neumann on 23.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TranslateNumbers.
+FOUNDATION_EXPORT double TranslateNumbersVersionNumber;
+
+//! Project version string for TranslateNumbers.
+FOUNDATION_EXPORT const unsigned char TranslateNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranslateNumbers/PublicHeader.h>
+
+
